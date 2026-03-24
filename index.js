f5b8cd32b9fc4dd14268fde1c7b9caf24e7a2101
rafaelval <artillero.satanas@gmail.com>
2026-03-24 09:59:43 -0500

    fix: use PORT env variable

--- index.js
+++ index.js
@@ -219,6 +219,7 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Servidor en http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Servidor en puerto ${PORT}`);
 });
